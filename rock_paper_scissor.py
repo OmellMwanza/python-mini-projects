a9f935e9f53b9c191836365bd941710872442bfd
OmellMwanza <omellmwanza55@gmail.com>
2026-06-10 23:25:53 +0200

    Modularized the Code & Refactored it :)

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -6,17 +6,19 @@ emojis = {
     's': '✂️'
 }
 
-while True:
+def get_user_choice():
     user_choice = input("Rock, Paper, or Scissors (r/p/s): ").lower()
-    if user_choice not in emojis:
+    if user_choice in emojis:
+        return user_choice
+    else:
         print("Invalid choice.")
-        continue
 
-    computer_choice = random.choice(list(emojis.keys()))
 
+def display_choices(user_choice, computer_choice):
     print(f'You chose {emojis[user_choice]}.')
     print(f'Computer chose {emojis[computer_choice]}.')
 
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie!")
 
@@ -29,9 +31,19 @@ while True:
     else:
         print("You lose!")
 
-    continue_choice = input("Continue? (y/n): ").lower()
-    if continue_choice != 'y':
-        print("Thanks for playing!")
-        break
-    else:
-        continue
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(list(emojis.keys()))
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        continue_choice = input("Continue? (y/n): ").lower()
+        if continue_choice != 'y':
+            print("Thanks for playing!")
+            break
+
+play_game()
